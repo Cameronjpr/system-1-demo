92ef5dd16dbebb85f77e7bd4e3992bfb4360f57f
Cameron Robson <cameronjpr@gmail.com>
2020-06-12 10:19:10 +0100

    Updates

---
+++ precache-manifest.b4a6658c59470af744169512af7ce662.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d1473b5e92e763b42b357e541f751e1",
+    "url": "/system-1-demo/index.html"
+  },
+  {
+    "revision": "387bf68839f2e179aa7d",
+    "url": "/system-1-demo/static/css/main.a7da0bef.chunk.css"
+  },
+  {
+    "revision": "472a4841e4f5b30b609a",
+    "url": "/system-1-demo/static/js/2.64f165d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/system-1-demo/static/js/2.64f165d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "387bf68839f2e179aa7d",
+    "url": "/system-1-demo/static/js/main.0a142e39.chunk.js"
+  },
+  {
+    "revision": "768398f2049e3d36e6c0",
+    "url": "/system-1-demo/static/js/runtime-main.1b16e534.js"
+  },
+  {
+    "revision": "e6bedc717c42d8066a53aecf3e1d0a7c",
+    "url": "/system-1-demo/static/media/ChiKareGo2.e6bedc71.ttf"
+  }
+]);
