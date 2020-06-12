16beefc802b5f380be54d07a2a478a1bd2c41cdb
Cameron Robson <cameronjpr@gmail.com>
2020-06-12 09:37:30 +0100

    initial commit

---
+++ src/features/message/AddMessage.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { postMessage } from './messageSlice'
+import {
+  StyledMessageForm,
+  StyledAddMessageInput,
+  StyledAddMessageButton,
+} from './AddMessage.styled'
+
+export function AddMessage(props) {
+  const dispatch = useDispatch()
+  const [text, setText] = useState(' ')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    dispatch(postMessage(text) || '')
+    setText('')
+  }
+
+  return (
+    <StyledMessageForm onSubmit={(e) => handleSubmit(e)}>
+      <StyledAddMessageInput
+        aria-label="Enter text"
+        value={text}
+        onChange={(e) => setText(''+e.target.value)}
+      />
+      <StyledAddMessageButton>Send</StyledAddMessageButton>
+    </StyledMessageForm>
+  )
+}
